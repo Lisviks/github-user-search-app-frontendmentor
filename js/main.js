49554df8f45c8e82ebc5c0df2249bb9eadad77fb
Deividas Rimkus <8516648+Lisviks@users.noreply.github.com>
2022-09-30 06:07:23 +0100

    save selected theme to local storage

--- js/main.js
+++ js/main.js
@@ -2,14 +2,36 @@ const body = document.querySelector('body');
 const themeBtn = document.querySelector('.theme-toggle');
 const themeBtnText = document.querySelector('.theme-toggle span');
 
+const setLightTheme = () => {
+  body.classList.remove('dark');
+  body.classList.add('light');
+  themeBtnText.innerText = 'Dark';
+  localStorage.setItem('devfinder-theme', 'light');
+};
+
+const setDarkTheme = () => {
+  body.classList.remove('light');
+  body.classList.add('dark');
+  themeBtnText.innerText = 'Light';
+  localStorage.setItem('devfinder-theme', 'dark');
+};
+
+const initTheme = () => {
+  const currentTheme = localStorage.getItem('devfinder-theme');
+
+  if (currentTheme === null || currentTheme === 'light') {
+    setLightTheme();
+  } else if (currentTheme === 'dark') {
+    setDarkTheme();
+  }
+};
+
+initTheme();
+
 themeBtn.addEventListener('click', () => {
   if (body.classList.contains('light')) {
-    body.classList.remove('light');
-    body.classList.add('dark');
-    themeBtnText.innerText = 'Light';
+    setDarkTheme();
   } else if (body.classList.contains('dark')) {
-    body.classList.remove('dark');
-    body.classList.add('light');
-    themeBtnText.innerText = 'Dark';
+    setLightTheme();
   }
 });
